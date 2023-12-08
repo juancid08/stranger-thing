17eb0632fea55dd34f9e0d9f337ecabbb01be565
juancid08 <145454462+juancid08@users.noreply.github.com>
2023-12-08 22:39:49 +0100

    Add files via upload

---
+++ Stranger things/js/index.js
@@ -0,0 +1,108 @@
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    
+    var button = document.getElementById('button');
+    var informacion = document.getElementById('informacion');
+    button.addEventListener('click', function () {
+        if (informacion.style.display === 'none') {
+            informacion.style.display = 'block';
+        } else {
+            informacion.style.display = 'none';
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  var button2 = document.getElementById('button2');
+  var informacion2 = document.getElementById('informacion2');
+  button2.addEventListener('click', function () {
+      if (informacion2.style.display === 'none') {
+          informacion2.style.display = 'block';
+      } else {
+          informacion2.style.display = 'none';
+      }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var button3 = document.getElementById('button3');
+  var informacion3 = document.getElementById('informacion3');
+
+  button3.addEventListener('click', function () {
+      if (informacion3.style.display === 'none') {
+          informacion3.style.display = 'block';
+      } else {
+          informacion3.style.display = 'none';
+      }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var button4 = document.getElementById('button4');
+  var informacion4 = document.getElementById('informacion4');
+
+  button4.addEventListener('click', function () {
+      if (informacion4.style.display === 'none') {
+          informacion4.style.display = 'block';
+      } else {
+          informacion4.style.display = 'none';
+      }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var button5 = document.getElementById('button5');
+  var informacion5 = document.getElementById('informacion5');
+
+  button5.addEventListener('click', function () {
+      if (informacion5.style.display === 'none') {
+          informacion5.style.display = 'block';
+      } else {
+          informacion5.style.display = 'none';
+      }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var button6 = document.getElementById('button6');
+  var informacion6 = document.getElementById('informacion6');
+
+  button6.addEventListener('click', function () {
+      if (informacion6.style.display === 'none') {
+          informacion6.style.display = 'block';
+      } else {
+          informacion6.style.display = 'none';
+      }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var button7 = document.getElementById('button7');
+  var informacion7 = document.getElementById('informacion7');
+
+  button7.addEventListener('click', function () {
+      if (informacion7.style.display === 'none') {
+          informacion7.style.display = 'block';
+      } else {
+          informacion7.style.display = 'none';
+      }
+  });
+});
+document.addEventListener('DOMContentLoaded', function () {
+  var button8 = document.getElementById('button8');
+  var informacion8 = document.getElementById('informacion8');
+
+  button8.addEventListener('click', function () {
+      if (informacion8.style.display === 'none') {
+          informacion8.style.display = 'block';
+      } else {
+          informacion8.style.display = 'none';
+      }
+  });
+});
